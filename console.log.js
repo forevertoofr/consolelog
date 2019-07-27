af1f7fe274390f3b0b22294b2d583205a522cf2c
YOLO R <52312596+haydenledean@users.noreply.github.com>
2019-07-27 08:57:40 +0200

    Delete console.log.js

--- console.log.js
+++
@@ -1,5 +0,0 @@
-console.log("Bienvenue dans CONSOLE.LOG !")
-console.log("On va tout de suite commencer avec une liste des chose qu'on peux faire dans la Console.")
-console.log("Tape le mot Log pour commencer.")
-console.log("Et tu peux aussi contribuer a CONSOLE.LOG sur GitHub.")
-console.log("Merci beaucoup utilisateur de Chrome !")
